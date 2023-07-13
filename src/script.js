b11a2b0aa071463c830c3a834e1e5b1b9c0ae2d1
arifrashidi <78742458+arifrashidi@users.noreply.github.com>
2023-07-13 09:02:06 +0800

    commit 2

---
+++ src/script.js
@@ -0,0 +1,29 @@
+
+
+//🔸 Modal window function
+
+function openMenu(){
+    document.body.classList.add("js-when-button-been-clicked")
+}
+
+function closeMenu() {
+    document.body.classList.remove('js-when-button-been-clicked')
+}
+
+//🔸 click project to go the project website function
+
+function hyperlink_project_treact() {
+    window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+}
+function hyperlink_project_library() {
+    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+}
+function hyperlink_project_guess_number() {
+    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+}
+function hyperlink_project_pig_game() {
+    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+}
+function hyperlink_project_countrypedia() {
+    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+}
