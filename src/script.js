794e6218c8b83169efcc18f24a73e5d4c83afb9d
arifrashidi <78742458+arifrashidi@users.noreply.github.com>
2023-07-13 23:37:48 +0800

    commit 3

--- src/script.js
+++ src/script.js
@@ -13,17 +13,44 @@ function closeMenu() {
 //🔸 click project to go the project website function
 
 function hyperlink_project_treact() {
-    window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+    if (window.innerWidth < 720) {
+        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+    }
+    else {
+        console.log("not hyperlink!");
+    }
 }
+
 function hyperlink_project_library() {
-    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+    if (window.innerWidth < 720) {
+        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+    }
+    else {
+        console.log("not hyperlink!");
+    }
 }
+
 function hyperlink_project_guess_number() {
-    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+    if (window.innerWidth < 720) {
+        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+    }
+    else {
+        console.log("not hyperlink!");
+    }
 }
 function hyperlink_project_pig_game() {
-    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+    if (window.innerWidth < 720) {
+        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+    }
+    else {
+        console.log("not hyperlink!");
+    }
 }
 function hyperlink_project_countrypedia() {
-    window.open('https://www.youtube.com/watch?v=Uig6djcy73c&ab_channel=MeatCanyon', '_blank');
+    if (window.innerWidth < 720) {
+        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
+    }
+    else {
+        console.log("not hyperlink!");
+    }
 }
