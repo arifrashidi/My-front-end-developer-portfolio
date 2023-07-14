d25d3f8e8a72824553ccf7ef00226cd3e2b76cec
arifrashidi <78742458+arifrashidi@users.noreply.github.com>
2023-07-14 11:42:33 +0800

    commit 5

--- src/script.js
+++ src/script.js
@@ -2,55 +2,20 @@
 
 //🔸 Modal window function
 
-function openMenu(){
+// ^ querySelector 
+const nav_open_window_button = document.querySelector(".nav_open_window_button");
+const nav_closed_window_button = document.querySelectorAll(".nav_closed_window_button");
+
+// ^ addEventListener 
+nav_open_window_button.addEventListener("click", function() {
     document.body.classList.add("js-when-button-been-clicked")
-}
+});
 
-function closeMenu() {
-    document.body.classList.remove('js-when-button-been-clicked')
-}
+nav_closed_window_button.forEach(el => {
+    el.addEventListener('click', function() {
+        document.body.classList.remove('js-when-button-been-clicked')
+        console.log("remove");
+    });
+});
 
-//🔸 click project to go the project website function
-
-function hyperlink_project_treact() {
-    if (window.innerWidth < 720) {
-        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
-    }
-    else {
-        console.log("not hyperlink!");
-    }
-}
-
-function hyperlink_project_library() {
-    if (window.innerWidth < 720) {
-        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
-    }
-    else {
-        console.log("not hyperlink!");
-    }
-}
-
-function hyperlink_project_guess_number() {
-    if (window.innerWidth < 720) {
-        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
-    }
-    else {
-        console.log("not hyperlink!");
-    }
-}
-function hyperlink_project_pig_game() {
-    if (window.innerWidth < 720) {
-        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
-    }
-    else {
-        console.log("not hyperlink!");
-    }
-}
-function hyperlink_project_countrypedia() {
-    if (window.innerWidth < 720) {
-        window.open('https://www.youtube.com/watch?v=8UsfL3PW8NQ&ab_channel=MeatCanyon', '_blank');
-    }
-    else {
-        console.log("not hyperlink!");
-    }
-}
+console.log("Thanks for visiting");
